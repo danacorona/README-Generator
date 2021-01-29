8a3d93aa0f59a2de76a969684ff6ce93f4f1edaf
Dana Corona <dana.d.corona@gmail.com>
2021-01-28 21:23:47 -0600

    Updated the index.js file to include all edits for the README Generator.

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# README-Generator
+# README Generator
+
+
+
+## Description
+The README Generator was created to help simplify the amount of time it takes to create a README file for their projects that they build.
+
+
+***
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributors](#contributors)
+* [Tests](#tests)
+* [Questions](#questions)
+* [License](#License)
+
+
+***
+## Installation
+For this project, please run an 'npm install' and once completed run the 'node index.js' in the command line to start the generator.
+
+
+***
+## Usage
+This generator will give the developers more time to focus on their projects and allow them to obtain a nice and clean README file.
+
+
+***
+## Contributors
+Dana Corona
+
+
+***
+## Tests
+There were no tests for this project.
+
+
+***
+## Questions
+For questions about this project please reach out to me!
+
+[My Github Account]('https://github.com/danacorona')
+
+[My Email]('dana.d.corona@gmail.com')
+
+
+***
+## License
+![Badge]('https://img.shields.io/badge/license-MIT-blue.svg')
+
+***
---
+++ index.js
@@ -0,0 +1,120 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
+
+//Prompts the user for questions in regards to the README file
+inquirer
+    .prompt ([
+        {
+            type:'input', 
+            message:'What is the title of your project?',
+            name: 'title',
+        },
+        {
+            type: 'input',
+            message: 'Give a description of you project',
+            name: 'description',
+        },
+        {
+            type: 'input',
+            message: 'What are the installation instructions for this project?',
+            name: 'installation',
+        },
+        {
+            type: 'input',
+            message: 'What is the usage of this project?',
+            name: 'usage',
+        },
+        {
+            type: 'input',
+            message: 'Who helped you to complete this project?',
+            name: 'contributors',
+        },
+        {
+            type: 'list',
+            message: 'Licences',
+            choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+            name: 'license' 
+        },
+        {
+            type: 'input',
+            message: 'Any Tests to be included in this project?',
+            name: 'tests',
+        },
+        {
+            type: 'input',
+            message: 'What is your GitHub account name?',
+            name: 'ghaccount',
+        },
+        {
+            type: 'input',
+            message: 'What is your email address?',
+            name: 'email',
+        },
+        
+    ]).then((data) => {
+        const markdown = generateReadme(data);
+        return writeFileAsync('README.md', markdown);
+
+    }).then (function () {
+        console.log("Successfully wrote Readme file");
+    }).catch (function (error){
+        console.log(error);
+    });
+        
+
+function generateReadme(data) {
+    return `# ${data.title}
+
+
+
+## Description
+${data.description}
+
+
+***
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributors](#contributors)
+* [Tests](#tests)
+* [Questions](#questions)
+* [License](#License)
+
+
+***
+## Installation
+${data.installation}
+
+
+***
+## Usage
+${data.usage}
+
+
+***
+## Contributors
+${data.contributors}
+
+
+***
+## Tests
+${data.tests}
+
+
+***
+## Questions
+For questions about this project please reach out to me!
+
+[My Github Account]('https://github.com/${data.ghaccount}')
+
+[My Email]('${data.email}')
+
+
+***
+## License
+![Badge]('https://img.shields.io/badge/license-${data.license}-blue.svg')
+
+***`
+};
